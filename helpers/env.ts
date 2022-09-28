c23a0ce93503edf83020e32819cdd88a7ba55efc
Aditya Gupta <adityagupta765469@gmail.com>
2022-09-28 18:15:04 +0530

    Update env.ts

--- helpers/env.ts
+++ helpers/env.ts
@@ -1,5 +1,12 @@
 export const getEnv = (): 'dev' | 'production' | 'local' => {
-  return 'local'
+  const envVar = process.env.NEXT_PUBLIC_XMTP_ENVIRONMENT
+  if (envVar === 'production') {
+    return envVar
+  }
+  if (envVar === 'local') {
+    return envVar
+  }
+  return 'dev'
 }
 
 export const tagStr = (): string | null => {
